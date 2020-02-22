831007c729bd7446c2b785d9283ff49f60baca7b
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-02-23 00:48:59 +0530

    added the git

---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'weatherApi.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List<Weather> places = [
+    Weather(cityName: "Kolkata"),
+    Weather(cityName: "NewDelhi"),
+    Weather(cityName: "Chennai"),
+    Weather(cityName: "Mumbai"),
+    Weather(cityName: "Tokyo"),
+  ];
+
+  List<String> images = [
+    "assets/animeMorning.jpg",
+    "assets/aestheticEvening.jpg",
+    "assets/animeMorning2.jpg",
+    "assets/DawnAnime.jpg",
+    "assets/FirewatchNight.jpg",
+  ];
+
+  List<String> displayImages = [];
+
+  int counter = 0;
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              
+              image: AssetImage("assets/bgimg.jpg"),
+              fit: BoxFit.fill
+            )
+          ),   
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                SizedBox(height: 5,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,                  
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.info),
+                      onPressed: () {},
+                      color: Colors.white60
+                    ),
+                    Text(
+                      "Weather",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white60
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.add),
+                      onPressed: () {},
+                      color: Colors.white60
+                    )
+                  ],
+                ),
+                SizedBox(height: 20,),
+                Expanded(            
+                  child: Container(                  
+                    child: Swiper(
+                      itemCount: displayImages.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 300,
+                          child: ClipRRect(                                                
+                            borderRadius: BorderRadius.circular(30),
+                            child: DecoratedBox(                          
+                              decoration: BoxDecoration(                            
+                                image: DecorationImage(                                  
+                                  image: AssetImage(displayImages[index]),
+                                  fit: BoxFit.fill
+                                )
+                              ),
+                            )  
+                          ),
+                        );
+                      },
+                      loop: false,
+                      scale: 0.8,
+                      viewportFraction: 0.72,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30,),
+                FloatingActionButton(
+                  onPressed: () {
+                    if(counter != images.length) {
+                      displayImages.add(images[counter]);
+                      setState(() {
+                        counter++;
+                      });
+                    }              
+                  },
+                  child: Icon(Icons.add),
+                  backgroundColor: Colors.amber[600],
+                ),
+                SizedBox(height: 30,),
+              ],
+            ),
+          ),
+        ),
+      )
+       
+    );
+  }
+
+  Widget photoCard(int index) {
+    return Container(
+      width: 300,
+      child: ClipRRect(                      
+        borderRadius: BorderRadius.circular(30),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(images[index]),
+              fit: BoxFit.fill
+            )
+          ),
+        )
+      ),
+    );
+  }  
+
+}
---
+++ lib/weatherApi.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class Weather {
+
+  String cityName;
+
+  Weather({this.cityName});
+
+  Future<void> getWeather() async {
+    // Api key = 9cc4d396352649cab1e283fea41427fd
+    Response response = await get("https://api.weatherbit.io/v2.0/forecast/daily?city=$cityName&key=9cc4d396352649cab1e283fea41427fd");
+    try {
+
+      Map data = jsonDecode(response.body);      
+      // print(data['data']);
+      List dataList = data['data'];
+      Map day1 = dataList[0];
+      Map day2 = dataList[1];
+      print('Values of day1 : ');
+      print(day1);
+      print('Values of day2 : ');
+      print(day2);
+
+    } catch (e) {  
+      print(e);
+      print('Could not retrieve data');
+    }
+
+  }
+
+}
