ba7defca4ceab3c6073066f8ab9dbc59292b0ea4
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-02-23 14:13:57 +0530

    checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'weatherApi.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   home: Home(),
 ));
 
@@ -13,7 +14,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  Weather obj = Weather(cityName: "Kolkata");
+  Weather obj = Weather(cityName: "Bangalore");
   // List<Weather> places = [
   //   Weather(cityName: "Kolkata"),
   //   Weather(cityName: "NewDelhi"),
@@ -40,155 +41,174 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Center(
         child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              
+          decoration: BoxDecoration(            
+            image: DecorationImage(              
               image: AssetImage("assets/bgimg.jpg"),
               fit: BoxFit.fill
             )
           ),   
-          child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                SizedBox(height: 5,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,                  
-                  children: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.info),
-                      onPressed: () {},
-                      color: Colors.white60
-                    ),
-                    Text(
-                      "Weather",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: "Montserrat",
-                        fontWeight: FontWeight.w400,
-                        letterSpacing: 2,
-                        color: Colors.white60
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () async {
-                        await obj.getWeather();
-                        images.shuffle();
-                        if(counter != images.length) {
-                          displayImages.add(images[0]);
-                          setState(() {
-                            counter = displayImages.length - 1;
-                          });
-                        }  
-                      },
-                      color: Colors.white60
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Expanded(            
-                  child: Container(                  
-                    child: Swiper(       
-                      onTap: (index) {
-                        // print(index);                        
-                      },               
-                      itemCount: displayImages.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          width: 300,
-                          child: ClipRRect(                       
-                            borderRadius: BorderRadius.circular(30),                            
-                            child: Container(
-                              decoration: BoxDecoration(                            
-                                image: DecorationImage(                                  
-                                  image: AssetImage(displayImages[index]),
-                                  fit: BoxFit.fill
-                                )
-                              ),
-                              child: Column(                          
-                                children: <Widget>[
-                                  SizedBox(height: 5,),
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 10),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: <Widget>[
-                                        Container(
-                                          height: 40,
-                                          width: 40,
-                                          child: RawMaterialButton(
-                                            shape: CircleBorder(),
-                                            onPressed: () {
-                                              setState(() {
-                                                displayImages.remove(displayImages[index]);
-                                              });
-                                              print(displayImages);
-                                            },                                            
-                                            child: Icon(Icons.remove, color: Colors.redAccent,),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                  SizedBox(height: 7,),
-                                  Text(
-                                    "${obj.cityName}, ${obj.countryCode}",
-                                    style: TextStyle(
-                                      fontFamily: "Montserrat",
-                                      fontSize: 35,
-                                      color: Colors.white70,
-                                      fontWeight: FontWeight.w200,
-                                      letterSpacing: 1
-                                    ),
-                                  ),
-                                  SizedBox(height: 2,),
-                                  Text(
-                                    "${obj.finalDate}",
-                                    style: TextStyle(
-                                      fontFamily: "Montserrat",
-                                      color: Colors.white70,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300
-                                    ),
-                                  ),
-                                  SizedBox(height: 50,),
-                                  Text(
-                                    "${obj.temp}°C",
-                                    style: TextStyle(
-                                      fontFamily: "Montserrat",
-                                      fontSize: 55,
-                                      fontWeight: FontWeight.w300,
-                                      color: Colors.white70                                       
-                                    ),
-                                  ),
-                                ],
-                              )
-                            ),
-                          ),
-                          
-                        );
-                      },
-                      loop: false,
-                      scale: 0.8,
-                      viewportFraction: 0.72,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(                  
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black.withOpacity(0.6),
+                  Colors.black.withOpacity(0.6),
+                ]
+              )
+            ),
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SizedBox(height: 5,),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,                  
                     children: <Widget>[
-                      Text("day1", style: TextStyle(color: Colors.white60),),
-                      Text("day2", style: TextStyle(color: Colors.white60),),
-                      Text("day3", style: TextStyle(color: Colors.white60),),
+                      IconButton(
+                        icon: Icon(Icons.info),
+                        onPressed: () {},
+                        color: Colors.white60
+                      ),
+                      Text(
+                        "Weather",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 2,
+                          color: Colors.white60
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.add),
+                        onPressed: () async {
+                          await obj.getWeather();
+                          images.shuffle();
+                          if(counter != images.length) {
+                            displayImages.add(images[0]);
+                            setState(() {
+                              counter = displayImages.length - 1;
+                            });
+                          }  
+                        },
+                        color: Colors.white60
+                      ),
                     ],
                   ),
-                ),
-                SizedBox(height: 30,),
-              ],
+                  SizedBox(height: 20,),
+                  Expanded(            
+                    child: Container(                                    
+                      child: Swiper(       
+                        onTap: (index) {
+                          // print(index);                        
+                        },               
+                        itemCount: displayImages.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            height: 100,
+                            width: 300,
+                            child: ClipRRect(                       
+                              borderRadius: BorderRadius.circular(30),                            
+                              child: Container(
+                                decoration: BoxDecoration(                                 
+                                  image: DecorationImage(                                  
+                                    image: AssetImage(displayImages[index]),
+                                    fit: BoxFit.fill
+                                  )
+                                ),
+                                child: Column(                          
+                                  children: <Widget>[
+                                    SizedBox(height: 5,),
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 10),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        children: <Widget>[
+                                          Container(
+                                            height: 40,
+                                            width: 40,
+                                            child: RawMaterialButton(
+                                              shape: CircleBorder(),
+                                              onPressed: () {
+                                                setState(() {
+                                                  displayImages.remove(displayImages[index]);
+                                                });
+                                                print(displayImages);
+                                              },                                            
+                                              child: Icon(Icons.remove, color: Colors.redAccent,),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(height: 7,),
+                                    Text(
+                                      "${obj.cityName}, ${obj.countryCode}",
+                                      style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        fontSize: 35,
+                                        color: Colors.white70,
+                                        fontWeight: FontWeight.w200,
+                                        letterSpacing: 1
+                                      ),
+                                    ),
+                                    SizedBox(height: 2,),
+                                    Text(
+                                      "${obj.finalDate}",
+                                      style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        color: Colors.white70,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w300
+                                      ),
+                                    ),
+                                    SizedBox(height: 50,),
+                                    Text(
+                                      "${obj.temp}°C",
+                                      style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        fontSize: 55,
+                                        fontWeight: FontWeight.w300,
+                                        color: Colors.white70                                       
+                                      ),
+                                    ),
+                                     Text(
+                                      "Feels like : ${obj.feelsLike}°C",
+                                      style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        color: Colors.white70,
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w300
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ),
+                            ),
+                            
+                          );
+                        },
+                        loop: false,
+                        scale: 0.85,
+                        viewportFraction: 0.72,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(                  
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,                  
+                      children: <Widget>[
+                        Text("day1", style: TextStyle(color: Colors.white60),),
+                        Text("day2", style: TextStyle(color: Colors.white60),),
+                        Text("day3", style: TextStyle(color: Colors.white60),),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                ],
+              ),
             ),
           ),
         ),
--- lib/weatherApi.dart
+++ lib/weatherApi.dart
@@ -7,7 +7,8 @@ class Weather {
   String cityName;
   String countryCode;
   String date;
-  double temp;
+  int temp;
+  double feelsLike;
   String currentMonth;
   String finalDate;
 
@@ -33,9 +34,14 @@ class Weather {
     try {
 
       // Api key = 9cc4d396352649cab1e283fea41427fd
-      Response response = await get("https://api.weatherbit.io/v2.0/forecast/daily?city=$cityName&key=9cc4d396352649cab1e283fea41427fd");
+      Response response1 = await get("https://api.weatherbit.io/v2.0/forecast/daily?city=$cityName&key=9cc4d396352649cab1e283fea41427fd");
+      Response response2 = await get("https://api.weatherbit.io/v2.0/current?city=$cityName&key=9cc4d396352649cab1e283fea41427fd");
 
-      Map data = jsonDecode(response.body);      
+      Map data = jsonDecode(response1.body);    
+      Map currentDatatemp = jsonDecode(response2.body);
+      List tempData = currentDatatemp['data'];
+      Map finalTempData = tempData[0];
+
       // print(data['data']);
       List dataList = data['data'];
       Map day0 = dataList[0];      
@@ -50,7 +56,9 @@ class Weather {
       }
       finalDate = "$currentMonth ${date.substring(8)}, ${date.substring(0, 4)}";      
       countryCode = data['country_code'];
-      temp = day0['temp'];
+      temp = finalTempData['temp'];
+      feelsLike = finalTempData['app_temp'];
+
       // print('Values of day1 : ');
       // print(day1);
       // print('Values of day2 : ');
