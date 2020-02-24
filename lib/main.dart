432da91684800cc233b03f8e33fb33a71cea9997
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-02-24 17:57:10 +0530

    added the weather icons

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,46 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
   
   int dayIndex = -1;
 
+  Map weatherIcons = {
+    "Thunderstorm with light rain" : "t01d.png",
+    "Thunderstorm with rain" : "t02d.png",
+    "Thunderstorm with heavy rain" : "t03d.png",
+    "Thunderstorm with light drizzle" : "t04d.png",
+    "Thunderstorm with drizzle" : "t04d.png",
+    "Thunderstorm with heavy drizzle" : "t04d.png",
+    "Thunderstorm with hail" : "t04d.png",
+    "Light drizzle" : "d01d.png",
+    "Drizzle" : "d01d.png",
+    "Heavy drizzle" : "d01d.png",
+    "Light rain" : "r01d.png",
+    "Moderate rain" : "r01d.png",
+    "Heavy rain" : "r03d.png",
+    "Freezing rain" : "r01d.png",
+    "Light shower rain" : "r01d.png",
+    "Shower rain" : "r05d.png",
+    "Heavy shower rain" : "r01d.png",
+    "Light snow" : "s01d.png",
+    "Snow" : "s02d.png",
+    "Heavy snow" : "s02d.png",
+    "Mix snow/rain" : "s01d.png",
+    "Sleet" : "s05d.png",
+    "Heavy sleet" : "s05d.png",
+    "Snow shower" : "s01d.png",
+    "Heavy snow shower" : "s02d.png",
+    "Flurries" : "s06d.png",
+    "Mist" : "a01d.png",
+    "Smoke" : "a01d.png",
+    "Haze" : "a01d.png",
+    "Sand/dust" : "a01d.png",
+    "Fog" : "a01d.png",
+    "Freezing Fog" : "a01d.png",
+    "Clear sky" : "c01d.png",
+    "Few clouds" : "c02d.png",
+    "Broken clouds" : "c03d.png",
+    "Overcast clouds" : "c04d.png",
+    "Unknown Precipitation" : "r01d.png",
+  };
+
 
   @override
   Widget build(BuildContext context) {
@@ -147,7 +187,7 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
                                       ),
                                       SizedBox(height: 7,),
                                       Padding(
-                                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                                        padding: const EdgeInsets.symmetric(horizontal: 6),
                                         child: AutoSizeText(
                                           "${cities[index].cityName[0].toUpperCase()}${cities[index].cityName.substring(1)}, ${cities[index].countryCode}",
                                           maxLines: 1,
@@ -198,13 +238,27 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
                                           color: Colors.white70
                                         ),
                                       ),
-                                      Text(
-                                        "${cities[index].description}",
-                                        style: TextStyle(
-                                          fontFamily: "Montserrat",
-                                          fontSize: 14,
-                                          letterSpacing: 1,
-                                          color: Colors.white70
+                                      // Text(
+                                      //   "${cities[index].description}",
+                                      //   style: TextStyle(
+                                      //     fontFamily: "Montserrat",
+                                      //     fontSize: 14,
+                                      //     letterSpacing: 1,
+                                      //     color: Colors.white70
+                                      //   ),
+                                      // ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 6),
+                                        child: AutoSizeText(
+                                          "${cities[index].description}",
+                                          maxLines: 1,
+                                          style: TextStyle(
+                                            fontFamily: "Montserrat",
+                                            fontSize: 14,
+                                            color: Colors.white70,
+                                            fontWeight: FontWeight.w200,
+                                            letterSpacing: 1
+                                          ),
                                         ),
                                       ),
                                       SizedBox(height: 5,),
@@ -244,13 +298,16 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,                          
                               children: <Widget>[
                                 Container(
+                                  width: 45,
+                                  height: 45,
                                   decoration: BoxDecoration(
                                     image: DecorationImage(
-                                      image: AssetImage("assets/a01d.png")
+                                      image: dayIndex == -1 ?
+                                        AssetImage("assets/c04d.png") :
+                                        AssetImage("assets/${weatherIcons["${cities[dayIndex].description}"]}")
                                     )
                                   ),
                                 ),
-                                SizedBox(height: 2,),
                                 Text(
                                   // "data",
                                   // "${cities[dayIndex].finalLowTemp}°C/${cities[dayIndex].finalHighTemp}°C",
@@ -274,7 +331,17 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
-                                Icon(Icons.cloud, color: Colors.white70,),
+                                Container(
+                                  width: 45,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: dayIndex == -1 ?
+                                        AssetImage("assets/c04d.png") :
+                                        AssetImage("assets/${weatherIcons["${cities[dayIndex].description}"]}")
+                                    )
+                                  ),
+                                ),
                                 SizedBox(height: 1,),
                                 Text(
                                   // "data",
@@ -298,7 +365,17 @@ class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
-                                Icon(Icons.cloud, color: Colors.white70,),
+                                Container(
+                                  width: 45,
+                                  height: 45,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: dayIndex == -1 ?
+                                        AssetImage("assets/c04d.png") :
+                                        AssetImage("assets/${weatherIcons["${cities[dayIndex].description}"]}")
+                                    )
+                                  ),
+                                ),
                                 SizedBox(height: 1,),
                                 Text(
                                   // "data",
